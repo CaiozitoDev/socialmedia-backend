2de78b925e8eb46e28a55ff1d07995f45572e344
Caio Felipe <imcaiofelipe@gmail.com>
2020-07-09 14:12:45 -0300

    Correção de bug na /messagelist

--- server.js
+++ server.js
@@ -582,13 +582,18 @@ app.get('/lastchat', (req, res) => {
 app.get('/messagelist', (req, res) => {
     const {db_user_id, notification} = req.query
 
-    chatCollection.find({'members.userid': `${db_user_id}`}).select({messages: true}).then(doc => {
+    chatCollection.find({'members.userid': `${db_user_id}`}).then(doc => {
         const notSawMessages = []
 
         doc.map(chat => {
             if(chat.messages[chat.messages.length - 1].userid !== db_user_id) {
                 const lastMessage = chat.messages[chat.messages.length - 1]
-                notSawMessages.push({...lastMessage, chatid: chat._id})
+                
+                if(chat.members[0].userid !== db_user_id) {
+                    notSawMessages.push({...lastMessage, chatid: chat._id, userPhoto: chat.members[0].userPhoto})
+                } else if(chat.members[1].useri !== db_user_id) {
+                    notSawMessages.push({...lastMessage, chatid: chat._id, userPhoto: chat.members[1].userPhoto})
+                }
             }
         })
         
