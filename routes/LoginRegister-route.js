4033ef182635d3367ea852e9f1fe2cf3590ce5d3
Caio Felipe <imcaiofelipe@gmail.com>
2020-12-05 19:02:21 -0300

    make it better

--- routes/LoginRegister-route.js
+++ routes/LoginRegister-route.js
@@ -74,7 +74,7 @@ route.post('/register', upload.single('photo'), (req, res, next) => {
                                 .then(() => {
                                     res.cookie('token', generatedToken, {
                                         httpOnly: true,
-                                        sameSite: false
+                                        sameSite: 'none'
                                     }).send({
                                         message: 'Registration successfully',
                                         authorized: true
@@ -122,7 +122,7 @@ route.post('/login', upload.any(), (req, res, next) => {
 
                             res.cookie('token', generatedToken, {
                                 httpOnly: true,
-                                sameSite: false
+                                sameSite: 'none'
                             }).send({
                                 message: 'Login successfully',
                                 authorized: true
