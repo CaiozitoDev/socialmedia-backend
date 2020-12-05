07221fa5eee43bff9c7233a3c5ad6c7e21b1ad05
Caio Felipe <imcaiofelipe@gmail.com>
2020-12-05 20:25:37 -0300

    make it better

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ io.on('connection', socket => {
 
 // MIDDLEWARES
 app.use(cors({
-    allowedHeaders: ['Authorization']
+    exposedHeaders: ['Authorization']
 }))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
