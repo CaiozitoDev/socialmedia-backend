942ff750ef113194990a51b41e76df222f1665d6
Caio Felipe <imcaiofelipe@gmail.com>
2020-12-13 00:00:57 -0300

    backend2.0

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http, {
     cors: {
-        origin: 'https://frontendtestedoteste.herokuapp.com' /* 'http://localhost:3000' */
+        origin: 'https://frontendtestedoteste.herokuapp.com'  /* 'http://localhost:3000' */
     }
 })
 
