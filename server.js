6560b9059cc9747eb078b6205ae15865cf97d13f
Caio Felipe <imcaiofelipe@gmail.com>
2020-12-13 16:32:44 -0300

    backend2.0

--- server.js
+++ server.js
@@ -35,8 +35,7 @@ io.on('disconnect', socket => {
 // MIDDLEWARES
 app.use(cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL,/* 'http://localhost:3000' */
-    preflightContinue: true
+    origin: '*',/* 'http://localhost:3000' */
 }))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
